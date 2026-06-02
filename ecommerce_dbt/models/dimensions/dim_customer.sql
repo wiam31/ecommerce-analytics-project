dc1d6ad8c0a2f63e5ec4a2b415d3425abb63d191
yasminebennouis <yasminebns04@gmail.com>
2026-06-02 21:43:42 +0100

    fix: add dbt tests and documentation

--- ecommerce_dbt/models/dimensions/dim_customer.sql
+++ ecommerce_dbt/models/dimensions/dim_customer.sql
@@ -1,5 +1,6 @@
-select distinct
+select
     cast(CustomerID as integer) as customer_id,
-    Country as country
+    max(Country) as country
 from read_csv_auto('../data/silver/online_retail_clean.csv')
 where CustomerID is not null
+group by CustomerID
